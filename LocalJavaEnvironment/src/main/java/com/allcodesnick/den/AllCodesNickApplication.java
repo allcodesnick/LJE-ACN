ffcd8592bfd48bbfa26f02a90b4d8fced9c18b2e
Nick Felix <62229340+allcodesnick@users.noreply.github.com>
2024-01-30 17:39:25 -0500

    Initialize release-v1

---
+++ LocalJavaEnvironment/src/main/java/com/allcodesnick/den/AllCodesNickApplication.java
@@ -0,0 +1,13 @@
+package com.allcodesnick.den;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllCodesNickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllCodesNickApplication.class, args);
+	}
+
+}
