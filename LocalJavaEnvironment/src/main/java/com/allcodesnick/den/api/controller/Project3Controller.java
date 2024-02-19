28c79d8088773788c8f77225090b24e2e3871a98
Nick Felix <allcodesnick@gmail.com>
2024-02-19 14:16:56 -0500

    projects

--- LocalJavaEnvironment/src/main/java/com/allcodesnick/den/api/controller/Project3Controller.java
+++ LocalJavaEnvironment/src/main/java/com/allcodesnick/den/api/controller/Project3Controller.java
@@ -1,13 +1,13 @@
-package com.allcodesnick.den.socialbuggsmmo.controller;
+package com.allcodesnick.den.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class Project3Controller {
 
-    @GetMapping("/social-buggs")
+    @GetMapping("/p3")
     public String login() {
-        return "Hello and Welcome to the Site";
+        return "Hello World";
     }
 }
